2631a48f1cef1a6007f5b5fc220f26e77a65a574
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2017-12-11 16:15:50 +0800

    站长之家页面爬取whois信息

--- crawl_zzhang.py
+++ crawl_zzhang.py
@@ -1,8 +1,12 @@
+# 通过http://whois.chinaz.com/网站来爬取whois信息
+
 import requests
 import time
 import re
+from bs4 import BeautifulSoup
 
-url = 'http://tool.chinaz.com/ipwhois?q='
+
+url = 'http://whois.chinaz.com/'
 headers = {
         'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
     }
@@ -11,11 +15,27 @@ def search_ip_info(ip):
 
     ip_url = url + ip
     web_data = requests.get(ip_url)
-    company_name = re.findall("<p>Name : (.*?)</p>",web_data.content.decode('utf-8'))
+    soup = BeautifulSoup(web_data.content,'lxml')
+    registrar = soup.select('#sh_info > li:nth-of-type(2) > div.fr.WhLeList-right > div > span')[0].get_text()
+    email = soup.select('#sh_info > li:nth-of-type(4) > div.fr.WhLeList-right.block.ball.lh24 > span')[0].get_text()
+    phone = soup.select('#sh_info > li:nth-of-type(5) > div.fr.WhLeList-right.block.ball.lh24 > span')[0].get_text()
+    creation_date = soup.select('#sh_info > li:nth-of-type(6) > div.fr.WhLeList-right > span')[0].get_text()
+    expiration_date = soup.select('#sh_info > li:nth-of-type(7) > div.fr.WhLeList-right > span')[0].get_text()
+    DNS = soup.select('#sh_info > li:nth-of-type(9) > div.fr.WhLeList-right')[0].get_text()
 
-
-    print(company_name)
+    data = {
+         # "公司":company,
+        # "地址":company_addr,
+        "注册商":registrar,
+        # "国家":country,
+        "邮箱":email,
+        "国家":phone,
+        "DNS":DNS,
+        "创建日期":creation_date,
+        "失效日期":expiration_date,
+    }
+    print(data)
 
 
 if __name__ == '__main__':
-    search_ip_info('8.8.8.8')
+    search_ip_info('www.qq.com')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#使用python - whois 模块
 import whois
 import time
 
@@ -10,16 +11,20 @@ def dealWithWhois(data):
     emails = data["emails"]
     creation_date = str(data["creation_date"][0])
     expiration_date = str(data["expiration_date"][0])
-
+    registrar  = data["registrar"]
+    dns = data["name_servers"]
     newData = {
         "公司":company,
         "地址":company_addr,
+        "注册商":registrar,
         "国家":country,
         "邮箱":emails,
+        "DNS":dns,
         "创建日期":creation_date,
-        "失效日期":expiration_date
+        "失效日期":expiration_date,
+
     }
     print(newData)
 if __name__ == '__main__':
-    tmp = whois.whois("www.qq.com")
+    tmp = whois.whois("www.sohu.com")
     dealWithWhois(tmp)
