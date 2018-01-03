6fc02428f4c715abceeab7ddc0968fb998be17c9
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2018-01-03 10:58:03 +0800

    改用提供的ip进行爬取

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from multiprocessing import Pool
 import csv
 
 # fp = None
-fp = open('geoip_mul_test.csv','w+',newline='',encoding='utf-8')
-write = csv.writer(fp)
-write.writerow(('IP','company','company_addr','country','emails','creation_date','expiration_date','registrar','dns'))
+
 def dealWithWhois(data):
 
     company = data["org"]
@@ -50,7 +48,8 @@ def get_whois_info(url):
             # write.writerow((url,tmp["org"],tmp["address"],tmp["country"],tmp["emails"],str(tmp["creation_date"]),str(tmp["expiration_date"]),tmp["registrar"],tmp["name_servers"]))
             write.writerow((url,'test'))
             result = dealWithWhois(tmp)
-            return result
+            print("###write success###:",result)
+            # return result
     except whois.parser.PywhoisError:
         print("The Registry database contains ONLY .COM, .NET, .EDU domains and Registrars.")
 
@@ -60,18 +59,33 @@ def get_whois_info(url):
 
 if __name__ == '__main__':
 
-    for x in range(100,256,1):
-        ip_list=[]
-        # fp = open('geoip_mul_{}.csv'.format(x),'w+',newline='',encoding='utf-8')
-        # write = csv.writer(fp)
-        # write.writerow(('IP','company','company_addr','country','emails','creation_date','expiration_date','registrar','dns'))
-        for y in range(0,256,1):
-            for z in range(0,256,1):
-                for k in range(0,256,1):
-                    ip = '{}.{}.{}.{}'.format(x,y,z,k)
-                    ip_list.append(ip)
-        pool = Pool(processes=4)
-        pool.map(get_whois_info,ip_list)
+    f = open('ip_test.txt','r',encoding='utf-8')
+    ip_list = []
+
+    # 将所有获取的ip放入list中
+    ip_str = f.read()
+    for tmp in ip_str.split(','):
+        ip_list.append(tmp)
+
+    f.close()
+
+    fp = open('geoip_mul_test.csv','w+',newline='',encoding='utf-8')
+    write = csv.writer(fp)
+    write.writerow(('IP','company','company_addr','country','emails','creation_date','expiration_date','registrar','dns'))
+
+    # for x in range(100,256,1):
+    #     ip_list=[]
+    #     # fp = open('geoip_mul_{}.csv'.format(x),'w+',newline='',encoding='utf-8')
+    #     # write = csv.writer(fp)
+    #     # write.writerow(('IP','company','company_addr','country','emails','creation_date','expiration_date','registrar','dns'))
+    #     for y in range(0,256,1):
+    #         for z in range(0,256,1):
+    #             for k in range(0,256,1):
+    #                 ip = '{}.{}.{}.{}'.format(x,y,z,k)
+    #                 ip_list.append(ip)
+
+    pool = Pool(processes=4)
+    pool.map(get_whois_info,ip_list)
     fp.close()
 
 
